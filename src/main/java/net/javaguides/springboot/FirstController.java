fc8e5db73592943d86d9c337f5398af82248cf32
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-25 13:48:52 -0500

    @GetMapping with @RequestParam like http://....?param1=value&param2=value

--- src/main/java/net/javaguides/springboot/FirstController.java
+++ src/main/java/net/javaguides/springboot/FirstController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,15 @@ public class FirstController {
 	@GetMapping("/hello/{user-name}")
 	public String pathVar(
 			@PathVariable("user-name") String userName
+	) {
+		return "my value = " + userName;
+	}
+
+	//hello?user-name=value&user-lastname=value
+	@GetMapping("/hello")
+	public String paramVar(
+			@RequestParam("user-name") String userName,
+			@RequestParam("user-lastname") String userLastName
 	) {
 		return "my value = " + userName;
 	}
