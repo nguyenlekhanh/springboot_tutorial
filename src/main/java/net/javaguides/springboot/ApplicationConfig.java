bdc7a7b1842f6538f57bcbede7446dc7a266e281
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-24 23:21:03 -0500

    using bean with @Quanlifier annotation

--- src/main/java/net/javaguides/springboot/ApplicationConfig.java
+++ src/main/java/net/javaguides/springboot/ApplicationConfig.java
@@ -1,13 +1,21 @@
 package net.javaguides.springboot;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 	
-	@Bean("mybean")
-	public MyFirstClass myFirstClass() {
+	@Bean
+	@Qualifier("bean1")
+	public MyFirstClass myFirstBean() {
 		return new MyFirstClass("First Bean");
+	}
+	
+	@Bean
+	@Qualifier("bean2")
+	public MyFirstClass mySecondBean() {
+		return new MyFirstClass("Second Bean");
 	}
 }
--- src/main/java/net/javaguides/springboot/MyFirstService.java
+++ src/main/java/net/javaguides/springboot/MyFirstService.java
@@ -1,15 +1,14 @@
 package net.javaguides.springboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
-	private MyFirstClass myFirstClass;
+	private final MyFirstClass myFirstClass;
 	
-	@Autowired
-	public MyFirstService(MyFirstClass myFirstClass) {
+	public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
 		this.myFirstClass = myFirstClass;
 	}
 	
