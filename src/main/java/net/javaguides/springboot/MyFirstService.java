a92e99af1c1bda9e14f874ddb3751805fc1ad76f
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-24 23:22:48 -0500

    using bean with @Primary annotation

--- src/main/java/net/javaguides/springboot/MyFirstService.java
+++ src/main/java/net/javaguides/springboot/MyFirstService.java
@@ -8,7 +8,7 @@ public class MyFirstService {
 
 	private final MyFirstClass myFirstClass;
 	
-	public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
+	public MyFirstService(MyFirstClass myFirstClass) {
 		this.myFirstClass = myFirstClass;
 	}
 	
