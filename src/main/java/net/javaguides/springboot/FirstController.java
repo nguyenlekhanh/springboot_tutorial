0e07a60c212b4d97223afd5e9793d91b817af36a
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-25 16:12:45 -0500

    deleteById

--- src/main/java/net/javaguides/springboot/FirstController.java
+++ src/main/java/net/javaguides/springboot/FirstController.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,13 @@ public class FirstController {
 	) {
 		
 		return repository.findAllByFirstNameContaining(name);
+	}
+	
+	@DeleteMapping("/student/{student-id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteById(
+			@PathVariable("student-id") Integer id
+	) {
+		repository.deleteById(id);
 	}
 }
