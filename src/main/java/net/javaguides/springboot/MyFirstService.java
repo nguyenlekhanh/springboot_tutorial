469df44be44dfcf585c95fec459bbbc72270aa11
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-24 23:29:39 -0500

    inject bean using function with parameter and @autowired @quanlifier

--- src/main/java/net/javaguides/springboot/MyFirstService.java
+++ src/main/java/net/javaguides/springboot/MyFirstService.java
@@ -7,9 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyFirstService {
 
-	@Autowired
-	@Qualifier("mySecondBean")
 	private MyFirstClass myFirstClass;
+	
+	@Autowired
+	public void injectDependencies (
+			@Qualifier("mySecondBean") MyFirstClass myFirstClass
+	) {
+		this.myFirstClass = myFirstClass;
+	}
 	
 	public String tellAStory() {
 		return "The Dependency is saying: " + myFirstClass.sayHello();
