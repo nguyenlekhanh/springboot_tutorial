d696a0eefde6ab10c269876bd6ea337b93d35554
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-25 15:30:54 -0500

    fix the connect to mysql create first student model with some annotations @Entity, @Id, @Column,...

---
+++ src/main/java/net/javaguides/springboot/Student.java
@@ -0,0 +1,84 @@
+package net.javaguides.springboot;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "T_STUDENT")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(
+			name="c_fname",
+			length=20
+	)
+	private String firstName;
+	@Column
+	private String lastName;
+	
+	@Column(unique=true)
+	private String email;
+	
+	@Column
+	private int age;
+	
+	@Column(updatable=false)
+	private int created_date;
+
+	public Student(Integer id, String firstName, String lastName, String email, int age) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.age = age;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
