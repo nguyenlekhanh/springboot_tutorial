48b4144d3f66c5dc9643ccabd011be1e45797932
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-25 13:23:39 -0500

    response status @ResponseStatus(HttpStatus.ACCEPTED)

--- src/main/java/net/javaguides/springboot/FirstController.java
+++ src/main/java/net/javaguides/springboot/FirstController.java
@@ -1,12 +1,15 @@
 package net.javaguides.springboot;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
 	
 	@GetMapping("/hello")
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	public String sayHello() {
 		return "Hello from my first controller";
 	}
