0ba8d7a9008ad41ba70947c3984fa44eb2381593
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-25 11:50:58 -0500

    bean using spring profile with @profile annotation

--- src/main/java/net/javaguides/springboot/ApplicationConfig.java
+++ src/main/java/net/javaguides/springboot/ApplicationConfig.java
@@ -4,16 +4,20 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class ApplicationConfig {
 	
 	@Bean
+	@Profile("dev")
 	public MyFirstClass myFirstBean() {
 		return new MyFirstClass("First Bean");
 	}
 	
 	@Bean
+	@Profile("test")
 	public MyFirstClass mySecondBean() {
 		return new MyFirstClass("Second Bean");
 	}
