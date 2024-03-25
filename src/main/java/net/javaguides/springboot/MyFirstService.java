9a9ecc8cc53f62f2db082add8dfaeb15e7376ead
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-24 23:24:45 -0500

    using @Autowired annotation replacing for constructor

--- src/main/java/net/javaguides/springboot/MyFirstService.java
+++ src/main/java/net/javaguides/springboot/MyFirstService.java
@@ -1,16 +1,14 @@
 package net.javaguides.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
-	private final MyFirstClass myFirstClass;
-	
-	public MyFirstService(MyFirstClass myFirstClass) {
-		this.myFirstClass = myFirstClass;
-	}
+	@Autowired
+	private MyFirstClass myFirstClass;
 	
 	public String tellAStory() {
 		return "The Dependency is saying: " + myFirstClass.sayHello();
