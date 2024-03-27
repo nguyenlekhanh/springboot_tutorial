195734dc8c182e9d2a63a6a6b7cb243b4466bb43
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-26 20:46:28 -0500

    write some test

--- src/main/java/net/javaguides/springboot/student/StudentMapper.java
+++ src/main/java/net/javaguides/springboot/student/StudentMapper.java
@@ -7,6 +7,9 @@ import net.javaguides.springboot.school.School;
 @Service
 public class StudentMapper {
 	public Student toStudent(StudentDto dto) {
+		if(dto == null) {
+			throw new NullPointerException("The student is null");
+		}
 		var student = new Student();
 		student.setFirstName(dto.firstName());
 		student.setLastName(dto.lastName());
--- src/test/java/net/javaguides/springboot/student/StudentMapperTest.java
+++ src/test/java/net/javaguides/springboot/student/StudentMapperTest.java
@@ -4,12 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StudentMapperTest {
 
+	private StudentMapper studentMapper;
+
+	
 //	@BeforeAll
 //	void beforeAll() {
 //		System.out.println("Inside the before all method");
@@ -22,7 +26,7 @@ class StudentMapperTest {
 	
 	@BeforeEach
 	void setUp() {
-		System.out.println("Inside the before each method");
+		studentMapper = new StudentMapper();
 	}
 	
 	@AfterEach
@@ -38,5 +42,27 @@ class StudentMapperTest {
 	@Test
 	public void testMethod2() {
 		System.out.println("My second test method");
+	}
+	
+	
+	@Test
+	public void shouldMapStudentDtoToStudent() {
+		StudentDto dto = new StudentDto("Test", "Test", "Test", 1, 1);
+		
+		Student student = studentMapper.toStudent(dto);
+		
+		Assertions.assertEquals(dto.firstName(), student.getFirstName());
+		Assertions.assertEquals(dto.lastName(), student.getLastName());
+		Assertions.assertEquals(dto.email(), student.getEmail());
+		Assertions.assertNotNull(student.getSchool());
+		Assertions.assertEquals(dto.schoolId(), student.getSchool().getId());
+		
+	}
+	
+	@Test
+	public void should_throw_null_pointer() {
+		//Assertions.assertThrows(NullPointerException.class, () -> studentMapper.toStudent(null));
+		var exp = Assertions.assertThrows(NullPointerException.class, () -> studentMapper.toStudent(null));
+		Assertions.assertEquals("The student is null", exp.getMessage());
 	}
 }
